b0e9f675320151caff87bee3de456b35ed6903b9
Abbas Noormohammadi <normohamadi1995@gmail.com>
2021-11-21 00:23:49 +0330

    setup navigation component

---
+++ app/src/main/java/ir/abbas/cryptocurrency/homeFragment/HomeFragment.java
@@ -0,0 +1,20 @@
+package ir.abbas.cryptocurrency.homeFragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ir.abbas.cryptocurrency.R;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
---
+++ app/src/main/java/ir/abbas/cryptocurrency/marketFragment/MarketFragment.java
@@ -0,0 +1,20 @@
+package ir.abbas.cryptocurrency.marketFragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ir.abbas.cryptocurrency.R;
+
+public class MarketFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_market, container, false);
+    }
+}
